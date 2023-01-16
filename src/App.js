775c3f6c0b28ede7f2fabc699868da7d14b325f6
Marcel Kristensen <marcel@geoprof.dk>
2023-01-16 23:30:05 +0100

    changed provider from index to app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Navbar from "./Components/Navbar";
 import Cart from "./Pages/Cart";
 import ProductData from "./Components/ProductData";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
 import Checkout from "./Pages/Checkout";
 
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<ProductData />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductData />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./Redux/store";
 import App from "./App";
 import "./index.css";
 
@@ -9,8 +7,6 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
